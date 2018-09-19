a53b13993c47657c365b58d8131d21c4df2b7b20
Erdem Bozkurt <ebozkurt93@gmail.com>
2018-09-19 21:34:44 +0300

    Finished entire project

--- server/app.js
+++ server/app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
+
+// connect to the mlab database
+mongoose.connect(
+  'mongodb://erdem:1133225544a@ds161102.mlab.com:61102/graphql-course'
+);
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 app.use(
   '/graphql',
